4ac14fa38ba60d743eb8c7e392dc4f18edecc557
Przemysław Komorowski <przemyslaw.komorowski91@gmail.com>
2022-11-09 23:58:26 +0100

    Added axios.post /addTask

--- client/src/components/InputData/InputField.tsx
+++ client/src/components/InputData/InputField.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InputField.min.css';
 import { FaBeer } from 'react-icons/fa';
+import axios from 'axios';
 
 const InputField: React.FC = () => {
-
+    const [name, setName] = useState<string>('');
+    const [price, setPrice] = useState<string>('');
 
     const addTask = (e: React.FormEvent) => {
         e.preventDefault();
-        console.log('test')
-      }
+        axios.post('http://localhost:9000/addTask', {
+            name: name,
+            price: price,
+            id: Math.random(),
+        })
+            .then((response) => {
+                console.log(`Added Thing ${name} ${price} PLN`)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        setName('');
+        setPrice('');
+     
+    }
+
+
     return (
         <form className='addTask' onSubmit={(e) => addTask(e)}>
-            <input type='text' maxLength={100} placeholder='Enter a name'  required></input>
-            <input type='number' placeholder='PLN' required></input>
+            <input type='text' maxLength={100} placeholder='Enter a name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required></input>
+            <input type='number' placeholder='PLN'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required></input>
             <button type='submit'>Add Task</button>
             <div><FaBeer /></div>
         </form>
