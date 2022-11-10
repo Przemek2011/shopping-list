46a4fee4a880efed6e66b207d04ac0e42758cacd
Przemysław Komorowski <przemyslawkomorowski91@gmail.com>
2022-11-10 17:56:56 +0100

    added options delete and update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -23,21 +23,21 @@ app.use(function (req, res, next) {
 
 // Data MySql
 const db = mysql.createConnection({
-    host: 'serwer2286904.home.pl',
-    user: '35797066_shopping_list',
-    password: 'shopping_list',
-    database: '35797066_shopping_list'
-})
-// Connect to MySQL
+        host: 'serwer2286904.home.pl',
+        user: '35797066_shopping_list',
+        password: 'shopping_list',
+        database: '35797066_shopping_list'
+    })
+    // Connect to MySQL
 db.connect(err => {
-if (err) {
-    throw err
-}
-console.log('MySQL Connected')
+    if (err) {
+        throw err
+    }
+    console.log('MySQL Connected')
 })
 
 //Create Table Shopping List
-app.get('/createShoppingList', (req, res) => {
+app.post('/createShoppingList', (req, res) => {
     let sql = 'CREATE TABLE ShoppingList(Id DOUBLE, Name VARCHAR(255), Price VARCHAR(255), UpdateDate VARCHAR(255) ,  PRIMARY KEY(id)) ';
     db.query(sql, err => {
         if (err) {
@@ -46,9 +46,7 @@ app.get('/createShoppingList', (req, res) => {
         res.send('Shopping List table created')
     })
 })
-app.get('/api', (req,res)=> {
-    res.send("hello")
-})
+
 
 // Add Thing
 app.post('/addTask', (req, res) => {
@@ -56,7 +54,7 @@ app.post('/addTask', (req, res) => {
         Id: req.body.id,
         Name: req.body.name,
         Price: req.body.price,
-     
+
     }
     let sql = 'INSERT INTO ShoppingList  SET?'
     let query = db.query(sql, post, err => {
@@ -67,8 +65,26 @@ app.post('/addTask', (req, res) => {
     })
 })
 
-
-
+// Download Shopping List
+app.get('/downloadShoppingList', (req, res) => {
+        let sql = 'SELECT * FROM ShoppingList'
+        let query = db.query(sql, (err, results) => {
+            if (err) {
+                throw err
+            }
+            res.send(results)
+        })
+    })
+    //Delete Thing
+app.delete('/deleteThing/:id', (req, res) => {
+    let sql = `DELETE FROM ShoppingList WHERE Id = ${req.params.id} `
+    let query = db.query(sql, err => {
+        if (err) {
+            throw err
+        }
+        res.send('Thing deleted')
+    })
+})
 
 app.listen(9000, () => {
     console.log('Server started on port 9000')
